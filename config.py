fa72b2b8cc8eb2f0d2a37bea467ecbf7406381ef
Yean1277 <78299273+Yean1277@users.noreply.github.com>
2023-09-07 17:41:11 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cv9sbdmnqyov.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "looyeanter-employee"
+customregion = "us-east-1"
+
